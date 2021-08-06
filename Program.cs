5a05897ce43395975788ec7cdf21913100883e23
Ivan <ivanscavalheiro@gmail.com>
2021-08-06 13:15:11 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strings = new List<string>();
+            var path = "/path/to/binary.exe";
+            var validChars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+            var currentStringBuilder = new StringBuilder();
+            using (var file = File.OpenRead(path))
+            {
+                while (file.Position < file.Length)
+                {
+                    var c = (char)file.ReadByte();
+                    if (validChars.IndexOf(c) >= 0)
+                    {
+                        currentStringBuilder.Append(c);
+                    }
+                    else
+                    {
+                        if (currentStringBuilder.Length > 0)
+                        {
+                            strings.Add(currentStringBuilder.ToString());
+                            currentStringBuilder = new StringBuilder();
+                        }
+                    }
+                }
+            }
+
+            if (currentStringBuilder.Length > 0)
+            {
+                strings.Add(currentStringBuilder.ToString());
+            }
+
+            Console.WriteLine("Possible RSA strings found:");
+            var query = strings.Where(x => x.Length == 256);
+
+            if (query.Count() == 0)
+            {
+                Console.WriteLine("No possible RSA strings found =S");
+            }
+
+            foreach (var s in query)
+            {
+                Console.WriteLine("> " + s);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Program to try and find RSA 256 keys in binary files
+
+To run it just make sure you have dotnet core (.net 5 >) installed, then update the "path" to point to the file you want to scan and then:
+
+```bash
+dotnet run
+```
